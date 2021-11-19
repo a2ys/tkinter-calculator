9a7cc282f4808461833b9ff0f597493a3f1eb748
Aayush Shukla <oreus.aks@gmail.com>
2021-11-19 17:43:21 +0530

    Update main.py
    Made slight changes to the main file. The final changes will be pushed in a day or two.

--- files/main.py
+++ files/main.py
@@ -1,30 +1,43 @@
-# -*- coding: utf- 8 -*-
-# TODO: Out of digits prompt and add mannual entry of operations functionality
+# TODO: Add mannual entry of operations functionality
 
 from tkinter import *
 from tkinter import font as tkFont
+from tkinter import messagebox
 import time
 
 root = Tk()
-root.grid_rowconfigure(0, weight=1)
-root.grid_columnconfigure(0, weight=1)
+root.geometry('700x600')
+root.grid_rowconfigure((0,1,2,3,4,5), weight=1)
+root.grid_columnconfigure((0,1,2,3), weight=1)
 root.title("CALCULATOR")
 root.resizable(False, False)
 
-segoe_font = tkFont.Font(family='Segoe UI', size=16)
-segoe_font_ac = tkFont.Font(family='Segoe UI', size=8)
+menubar = Menu(root)
+
+comwindow = Menu(menubar, tearoff=0)
+comwindow.add_command(label="Save Logs")
+comwindow.add_command(label="Quit", command=root.quit)
+menubar.add_cascade(label="Commands", menu=comwindow)
+
+helpmenu = Menu(menubar, tearoff=0)
+helpmenu.add_command(label="About the buttons")
+helpmenu.add_command(label="About the project")
+helpmenu.add_separator()
+helpmenu.add_command(label="Get source code")
+menubar.add_cascade(label="About", menu=helpmenu)
+
+segoe_font = tkFont.Font(family='Segoe UI', size=28)
+segoe_font_ac = tkFont.Font(family='Segoe UI',weight='bold', size=28)
 
 entry_text = StringVar()
-inout = Entry(root, textvariable=entry_text)
+inout = Entry(root, textvariable=entry_text, font=segoe_font)
 inout.grid(row=0, column=0, columnspan=4, sticky="nsew")
 operation = ''
 spl_char = "~!@#$%^&*()_+`{}|[]\\:\";'<>?,/="
 
-s = '⚠'
-a = s.encode('utf-8')
-
 num1 = 0
 result = 0
+tempst = ''
 operator = None
 
 def clear():
@@ -36,7 +49,7 @@ def allclear():
     operator=""
 
 def equals():
-    global num1, operator
+    global num1, operator, tempst
 
     try:
         tempvar = entry_text.get()
@@ -48,8 +61,8 @@ def equals():
                 num1 = answer
                 operator = None
             else:
-                answer = a.decode()
-                entry_text.set(answer)
+                entry_text.set('')
+                messagebox.showerror('Division Error', 'Cannot divide by 0')
                 num1 = 0
                 operator = None
         elif operator == '*':
@@ -68,12 +81,19 @@ def equals():
             num1 = answer
             operator = None
 
-        tempvar2 = float(entry_text.get())
-        if int(tempvar2) == tempvar2:
-            entry_text.set(int(tempvar2))
+        tempvar2 = str(entry_text.get())
+        if len(tempvar2) == 0:
+            entry_text.set('')
         else:
-            entry_text.set(round(tempvar2, 10))
-
+            if int(float(tempvar2)) == float(tempvar2):
+                if len(tempvar2) > 10:
+                    entry_text.set('')
+                    messagebox.showwarning('Out of digits', f'The answer cannot be printed as its length is greater than 10.\nAnswer is : {int(float(tempvar2))}')
+                else:
+                    entry_text.set(int(float(tempvar2)))
+                    print(len(tempvar2))
+            else:
+                entry_text.set(round(float(tempvar2), 5))
     except ValueError:
         pass
 
@@ -177,8 +197,6 @@ def decimal():
 
 def character_limit_and_check_entered_value(entry_text):
     if len(entry_text.get()) > 0:
-        entry_text.set(entry_text.get()[:20])
-
         for i in entry_text.get():
             if i.isalpha() or (i in spl_char):
                 l = entry_text.get()
@@ -187,23 +205,24 @@ def character_limit_and_check_entered_value(entry_text):
                 entry_text.set(toset)
 
 button18 = Button(root, text="AC", command=allclear, font=segoe_font_ac).grid(row=1, column=0, sticky="nsew")
-button1 = Button(root, text="C", command=clear, font=segoe_font).grid(row=1, column=1, sticky="nsew")
-button2 = Button(root, text="/", command=divide, font=segoe_font).grid(row=1, column=2, sticky="nsew")
+button1 = Button(root, text="C", command=clear, font=segoe_font_ac).grid(row=1, column=1, sticky="nsew")
+button2 = Button(root, text="/", command=divide, font=segoe_font_ac).grid(row=1, column=2, sticky="nsew")
 button3 = Button(root, text="×", command=multiply, font=segoe_font).grid(row=1, column=3, sticky="nsew")
 button5 = Button(root, text="7", command=tsev, font=segoe_font).grid(row=2, column=0, sticky="nsew")
 button6 = Button(root, text="8", command=teig, font=segoe_font).grid(row=2, column=1, sticky="nsew")
 button7 = Button(root, text="9", command=tnin, font=segoe_font).grid(row=2, column=2, sticky="nsew")
-button4 = Button(root, text="-", command=minus, font=segoe_font).grid(row=2, column=3, sticky="nsew")
+button4 = Button(root, text="-", command=minus, font=segoe_font_ac).grid(row=2, column=3, sticky="nsew")
 button9 = Button(root, text="4", command=tfou, font=segoe_font).grid(row=3, column=0, sticky="nsew")
 button10 = Button(root, text="5", command=tfiv, font=segoe_font).grid(row=3, column=1, sticky="nsew")
 button11 = Button(root, text="6", command=tsix, font=segoe_font).grid(row=3, column=2, sticky="nsew")
-button8 = Button(root, text="+", command=plus, font=segoe_font).grid(row=3, column=3, sticky="nsew")
+button8 = Button(root, text="+", command=plus, font=segoe_font_ac).grid(row=3, column=3, sticky="nsew")
 button12 = Button(root, text="1", command=tone, font=segoe_font).grid(row=4, column=0, sticky="nsew")
 button13 = Button(root, text="2", command=ttwo, font=segoe_font).grid(row=4, column=1, sticky="nsew")
 button14 = Button(root, text="3", command=tthr, font=segoe_font).grid(row=4, column=2, sticky="nsew")
-button15 = Button(root, text="=", command=equals, font=segoe_font).grid(row=4, column=3, rowspan=2, sticky="nsew")
+button15 = Button(root, text="=", command=equals, font=segoe_font_ac).grid(row=4, column=3, rowspan=2, sticky="nsew")
 button16 = Button(root, text="0", command=tzer, font=segoe_font).grid(row=5, column=0, columnspan=2, sticky="nsew")
-button17 = Button(root, text=".", command=decimal, font=segoe_font).grid(row=5, column=2, sticky="nsew")
+button17 = Button(root, text=".", command=decimal, font=segoe_font_ac).grid(row=5, column=2, sticky="nsew")
 
 entry_text.trace("w", lambda *args: character_limit_and_check_entered_value(entry_text))
+root.config(menu=menubar)
 root.mainloop()
